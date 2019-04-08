45c9a0222e0e6a7b2d261180965da5012e64e14c
Student <example@example.com>
2019-04-08 16:29:01 -0700

    add github service

---
+++ src/app/github.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class GithubService {
+
+  constructor(private http: Http) { }
+
+  getProfile(){
+    return this.http.get('https://api.github.com/users/grobergm/repos');
+  }
+
+}
--- src/app/home-page-profile-tile/home-page-profile-tile.component.ts
+++ src/app/home-page-profile-tile/home-page-profile-tile.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../github.service';
 
 @Component({
   selector: 'app-home-page-profile-tile',
   templateUrl: './home-page-profile-tile.component.html',
-  styleUrls: ['./home-page-profile-tile.component.css']
+  styleUrls: ['./home-page-profile-tile.component.css'],
+  providers: [GithubService]
 })
 export class HomePageProfileTileComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(public githubService:GithubService) { }
+  profile;
   ngOnInit() {
+    this.setProfileInfo();
+  }
+
+  setProfileInfo(){
+    this.githubService.getProfile().subscribe(response=>{
+      this.profile=response.json();
+      console.log("thisprofile",this.profile);
+    });
   }
 
 }

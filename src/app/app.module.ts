bd238074040c97bf488379ee9b2773d6fe58ecea
Matt Groberg <grobergmg@gmail.com>
2019-04-10 10:55:44 -0700

    add route to repo-detail page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { EditDetailComponent } from './edit-detail/edit-detail.component';
 import { RepoListComponent } from './repo-list/repo-list.component';
 import { GithubLoginComponent } from './github-login/github-login.component';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { RepoDetailComponent } from './repo-detail/repo-detail.component';
 
 
 
@@ -47,6 +48,7 @@ export const firebaseConfig = {
     EditDetailComponent,
     RepoListComponent,
     GithubLoginComponent,
+    RepoDetailComponent,
 
   ],
   imports: [
